c8212e43cece817028ae2c69bc3cee7d0dd2958a
JesusGargallo <71832344+JesusGargallo@users.noreply.github.com>
2020-10-11 22:59:02 +0200

    Add files via upload

--- exercicio2/exercicio2/src/exercicio2/Exercicio2.java
+++ exercicio2/exercicio2/src/exercicio2/Exercicio2.java
@@ -8,16 +8,22 @@ import javax.crypto.SecretKey;
 public class Exercicio2 {
 
      public static void main(String[] args) throws NoSuchAlgorithmException {
-        byte [] encoded=keygenKeyGeneration(128).getEncoded();
+        byte [] code=keygenKeyGeneration(128).getEncoded();
 		
-        for (int i=0; i<encoded.length; i++) {
-            System.out.println(Integer.toBinaryString(encoded[i] & 0xFF).replace(' ', '0')+ "-"+ encoded[i] );
+        for (int i=0; i<code.length; i++) {
+            System.out.println(Integer.toBinaryString(code[i] & 0xFF).replace(' ', '0')+ "-"+ code[i] );
         }
             System.out.println("Algoritme 2:");
-        byte [] encoded2=keygenKeyGeneration2(56).getEncoded();
+        byte [] code2=keygenKeyGeneration2(56).getEncoded();
 		
-        for (int j=0; j<encoded2.length; j++) {
-            System.out.println(Integer.toBinaryString(encoded2[j] & 0xFF).replace(' ', '0')+ "-"+ encoded2[j] );
+        for (int j=0; j<code2.length; j++) {
+            System.out.println(Integer.toBinaryString(code2[j] & 0xFF).replace(' ', '0')+ "-"+ code2[j] );
+        }
+            System.out.println("Algoritme 3:");
+        byte [] code3=keygenKeyGeneration3(256).getEncoded();
+		
+        for (int j=0; j<code3.length; j++) {
+            System.out.println(Integer.toBinaryString(code3[j] & 0xFF).replace(' ', '0')+ "-"+ code3[j] );
         }
     }
     public static  SecretKey keygenKeyGeneration(int keySize) {
@@ -53,6 +59,23 @@ public class Exercicio2 {
 	}
 	return sKey;
 	} 
+        public static  SecretKey keygenKeyGeneration3(int keySize) {
+            SecretKey sKey = null  ;
+		
+            if ((keySize == 56 )|| (keySize == 192)|| ( keySize == 256)) {
+		try {
+			KeyGenerator kgen = KeyGenerator.getInstance("HmacSHA256");
+			kgen.init(keySize);
+			sKey= kgen.generateKey();
+				
+		}catch (NoSuchAlgorithmException ex) {
+			System.err.println("Generador no disponible" );
+				
+		}
+	}
+	return sKey;
+	} 
+        
     }
     
 
